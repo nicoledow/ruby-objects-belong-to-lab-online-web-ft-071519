38ccf485db47f45d1feda1567a7c0f41159a9f60
Nicole Dow <nikki.dow71@gmail.com>
2019-07-29 14:09:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
   attr_accessor :title
   
-  def initialize (title, artist)
-    #@title = title
-    @artist = Artist.new(name)
-  end
+  # def initialize (title, artist)
+  #   #@title = title
+  #   @artist = Artist.new(name)
+  # end
 end
